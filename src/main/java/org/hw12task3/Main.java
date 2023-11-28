5a5641f6b8ab42e640e3d09f20142312369670ee
OleksandrGrigoriev <alexander.grigoriev.121987@gmail.com>
2023-11-28 17:05:24 +0200

    implement Task 3

---
+++ src/main/java/org/hw12task3/Main.java
@@ -0,0 +1,20 @@
+package org.hw12task3;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Operation add = (a, b) -> a + b;
+        Operation sub = (a, b) -> a - b;
+        Operation mul = (a, b) -> a * b;
+        Operation div = (a, b) -> (double) a / b;
+        Operation pow = (a, b) -> Math.pow(a, b);
+        Operation nRoot = (a, b) -> Math.round(Math.pow(a, (double) 1 / b));
+
+        System.out.println(add.provideOperation(125, 5));
+        System.out.println(sub.provideOperation(10, 1));
+        System.out.println(mul.provideOperation(10, 10));
+        System.out.println(div.provideOperation(64, 8));
+        System.out.println(pow.provideOperation(2, 8));
+        System.out.println(nRoot.provideOperation(125,3));
+    }
+}
---
+++ src/main/java/org/hw12task3/Operation.java
@@ -0,0 +1,5 @@
+package org.hw12task3;
+
+public interface Operation {
+    double provideOperation(int a, int b);
+}
